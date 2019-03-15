dcbc7701e22c66f08353b4ea569fc909b176282c
aa9955456 <aa9955456@gmail.com>
2019-03-15 11:47:31 +0800

    練習中

--- Gittutorial/GitTutorial/GitTutorial.cpp
+++ Gittutorial/GitTutorial/GitTutorial.cpp
@@ -2,8 +2,20 @@
 #include <string>
 #include <iostream>
 using namespace System;
-void Guess(int number) {
+bool Guess(int number) {
+	static int target = -1;
+	if (target == -1) {
+		Random r;
+		target = r.Next() % 100 + 1;
+	}
+	if (number == target)return true;
+	return false;
 }
 int main(array<System::String ^> ^args)
 {
+	int guess;
+	do {
+		std::cin >> guess;
+	} while (!Guess(guess));
+	return 0;
 }
